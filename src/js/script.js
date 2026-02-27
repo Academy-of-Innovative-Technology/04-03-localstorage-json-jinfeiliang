87a997f50de5533985a3e3830687d41fdaa3ce34
jinl33 <jinl33@nycstudents.net>
2026-02-27 13:56:53 -0500

    Loading and Saving Localstorage and Fetching if not found

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,47 @@
 var Database = {
-	keyName: "Mutant Database",
-	data: []
+  keyName: "Mutant Database",
+  data: [],
 };
 
 
-function loadDataSource() {
+async function Get_Data_From_Localstorage() {
+  try {
+    const response = await fetch("api.json");
+    if (!response.ok) {
+      throw new Error(`Reponse status: ${response.status}`);
+    }
 
-	
-	displayData( Database.data );
+    const result = await response.json();
+    Database.data = result.response;
+	console.log(Database);
+    //return result;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-
+async function loadDataSource() {}
 
 function displayData(dataArray) {
-
-
+  console.log(dataArray);
 }
 
-loadDataSource();
+function Save_Data() {
+	if (Database.data != [] && Database.data != "" && Database.data != "") {
+		let Formatted = {"response": Database.data}
+		localStorage.setItem(Database.keyName, JSON.stringify(Formatted))
+	}
+}
+
+function Load_Data() {
+  let Data = JSON.parse(localStorage.getItem(Database.keyName));
+  if (Data != [] && Data != undefined && Data != "") {
+    Database.data = Data;
+  } else {
+    Get_Data_From_Localstorage().then(() => {
+      loadDataSource();
+	  console.log(Database.data);
+    });
+  }
+}
+Load_Data();
